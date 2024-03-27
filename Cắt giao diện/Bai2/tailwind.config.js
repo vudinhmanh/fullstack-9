236f3e97245fa891ccd2eff4a2baac197dd5f3aa
vudinhmanh <manhmanhmanh289@gmail.com>
2024-03-27 14:36:01 +0700

    update

--- Cắt giao diện/Bai2/tailwind.config.js
+++ Cắt giao diện/Bai2/tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    mode: 'jit',
+    purge:["./*html"],
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily:{
+          dm:['DM Sans', 'sans-serif'],
+        },
+        colors:{
+          orangeff9:"#FF9900",
+          orangef8:"#F85A47",
+          gray31:"#31353B",
+          grayda:"#DADADA",
+        },
+      },
     },
     plugins: [],
   }
