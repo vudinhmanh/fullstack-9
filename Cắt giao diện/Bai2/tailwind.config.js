ef16e131b0f8abc847b7598513a9fe2ff8f05667
vudinhmanh <manhmanhmanh289@gmail.com>
2024-03-28 15:01:34 +0700

    done layout

--- Cắt giao diện/Bai2/tailwind.config.js
+++ Cắt giao diện/Bai2/tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
       extend: {
         fontFamily:{
           dm:['DM Sans', 'sans-serif'],
+          roboto:['Roboto','sans-serif'],
         },
         colors:{
           orangeff9:"#FF9900",
           orangef8:"#F85A47",
           gray31:"#31353B",
+          gray333:"#333",
           grayda:"#DADADA",
         },
       },
